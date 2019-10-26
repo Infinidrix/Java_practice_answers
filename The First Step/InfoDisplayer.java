47fd5fbe52da6d43d2a80bf5158d86bfb2b1eb85
Biruk Solomon <biruksolomon11@gmail.com>
2019-10-26 08:20:12 +0300

    Added the first 3 chapters

---
+++ The First Step/InfoDisplayer.java
@@ -0,0 +1,6 @@
+public class InfoDisplayer{
+	public static void main(String[] args){
+		System.out.println("Name: Biruk Solomon\nAddress: Figa, Addis Ababa\n");
+		System.out.println("Telephone: 091111111");
+	}
+}
